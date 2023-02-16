75c131ac3627d05c5502c1bedbea2dc25607b541
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-02-16 12:02:07 +0100

    Update

--- 4IA Grupa 2/2023.02.16 Lekcja.py
+++ 4IA Grupa 2/2023.02.16 Lekcja.py
@@ -1,1 +1,10 @@
-#
+a  = input("Podaj liczbę: ") # a = int(input("Podaj liczbę: "))
+a = int(a)
+print(a,type(a))
+
+if a > 0:
+    print("Liczba dodania")
+elif a < 0:
+    print("Liczba ujemna")
+else:
+    print("Liczba zero")
