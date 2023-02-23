40f2a2e275a35f7237b9cda9a75077c4f50ae3d9
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-02-23 11:51:25 +0100

    Przykłady pętli

---
+++ 4IA Grupa 2/2023.02.23 Lekcja.py
@@ -0,0 +1,17 @@
+#pętla while
+
+i=0
+while i<100:
+    print(i)
+    i+=1
+
+print("----------")
+
+#pętla for in
+for i in range(100):
+    print(i)
+
+print("----------")
+
+for i in range(30,100,13):  #generuje zestaw liczba od 30 do 99 co 13
+    print(i)
---
+++ 4IA Grupa 2/2023.02.23 Operatory.py
@@ -0,0 +1,30 @@
+a = 3
+b = 5
+#operatory arytmetyczne
+print(-a,+b)
+print(a+b)
+print(a-b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a*b)
+print(a**b)
+
+#operatory tekstowe
+a = "tekst1"
+b = "tekst2"
+print(a+b) #konkatenacja tekstów
+print(a*3) #powielanie tekstu
+
+#operatory relacji
+print(a==b)
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+print(a!=b)
+
+#operator przypisania
+a = 3
+a += 2  #a = a + 2 
+#itd...
