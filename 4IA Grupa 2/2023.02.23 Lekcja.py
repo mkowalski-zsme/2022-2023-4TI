5b264c09c753454d548d29899854c661db52b94f
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-02-23 12:09:15 +0100

    przykład

--- 4IA Grupa 2/2023.02.23 Lekcja.py
+++ 4IA Grupa 2/2023.02.23 Lekcja.py
@@ -15,3 +15,34 @@ print("----------")
 
 for i in range(30,100,13):  #generuje zestaw liczba od 30 do 99 co 13
     print(i)
+
+#przykład z lekcji
+
+x = int(input("Podaj liczbę: "))
+print("Podana liczba (",x,") jest:")
+if x<0:
+    print("ujemna,",end=" ")
+elif x>0:
+    print("dodania,",end=" ")
+else:
+    print("równa zero,",end=" ")
+
+if x%2 == 0:
+    print("parzysta,",end=" ")
+else:
+    print("nieparzysta,",end=" ")
+
+if x>1:
+    i = 2
+    f = True
+    while i*i<=x:
+        if x % i == 0:
+            f = False
+            break
+        i+=1
+
+    if f:
+        print("pierwsza")
+    else:
+        print("złożona")
+
