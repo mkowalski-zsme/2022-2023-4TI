d3baf8b96f630d91feba4c317e38ed6009456df2
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-02-23 10:42:48 +0100

    operatory

---
+++ 4IB Grupa 2/2023.02.23 lekcja.py
@@ -0,0 +1,23 @@
+a = 3
+b = 5
+#operatory arytmetyczne
+print(-a,+b)
+print(a+b)
+print(a-b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a*b)
+print(a**b)
+
+#operatory tekstowe
+a = "tekst1"
+b = "tekst2"
+print(a+b) #konkatenacja tekstów
+print(a*3) #powielanie tekstu
+
+#operator przypisania
+a = 3
+
+
+
