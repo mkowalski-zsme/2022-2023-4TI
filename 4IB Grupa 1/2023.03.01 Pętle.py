8a64dc2718d204d9c493ca4f32409e5ccf96f91d
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-03-01 11:21:31 +0100

    Lekcja

---
+++ 4IB Grupa 1/2023.03.01 Pętle.py
@@ -0,0 +1,27 @@
+i = -100
+while i<100:
+    print(i,end=",")
+    i+=3
+
+print("\n--------")
+
+for i in range(-100,100,3):
+    print(i,end=",")
+
+print("\n--------")
+
+for i in [2,3,5,3,2,4,5,3,2]:
+    print(i,end=",")
+
+print("\n--------")
+
+for i in "jakis randomowy tekst":
+    print(i,end=",")
+
+#1. napisz program, który wypisuje n liczb na ekran (n podane z klawiatury)
+
+#2. napisz program, który oblicza sumę liczb z zakresu <a,b) (a i b podane z klawiatury)
+
+#3. za pomocą pętli narysuj obszar o rozmiarze NxN zapełniony znakiami *
+
+#4. za pomocą pętli i instrukcji warunkowej narysuj litery: T, L, E, U, F N Z X 
