e1beea657de200c27c993cf1dab6da2f39d3e990
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-03-02 10:25:23 +0100

    Update

--- 4IB Grupa 2/2023.03.02 Funkcje.py
+++ 4IB Grupa 2/2023.03.02 Funkcje.py
@@ -44,6 +44,8 @@ print(SumaEl([2,3,4,65,4,3,4,5,6]))
 def Sum(a,b):
     return float(a) + float(b)
 
+print(Sum("3.14",7))
+
 #2. Zdefinuj funkcję, która sprawdza znak liczby i wypisuje odpowiednią informację na ekran
 def Sign(a):
     if a>0: print("Dodatnia")
@@ -52,14 +54,17 @@ def Sign(a):
 
 #3. Zdefinuj funkcję, która sprawdza czy z podanych długości odcinków można utworzyć trójkąt (False/True)
 def Triangle(a,b,c):
-    if a>b+c and b>a+c and c>a+b: return True
+    if a<b+c and b<a+c and c<a+b: return True
     else: return False
 
+print(Triangle(3,4,7))
+
 #4. Zdefinuj funkcję, która sprawdza czy podana liczba jest pierwsza
-def Prime(a):
-    if a<2: return False
-    for i in range(2, int((a**.5)+1)):
-        if a%i == 0: return False
+def Prime(n):
+    if n<2: return False
+    #for i in range(2, n):   #dzielniki z zakresu <2,n)
+    for i in range(2, int((n ** 0.5)+1)):  #dzielniki z zakresu <2,pierwiastek z n + 1)
+        if n%i == 0: return False
     return True
 
 for i in range(100):
@@ -75,12 +80,10 @@ def NWD(a,b):
 print(NWD(5,5))
 
 def NWD2(a,b):
-    while True:
+    while b:
         r = a%b
-        if r == 0: return b
-        else: 
-            a=b
-            b=r
-    return b
+        a = b
+        b = r        
+    return a
 
 print(NWD2(51,15))
