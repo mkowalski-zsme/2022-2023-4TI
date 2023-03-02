4ccc0be2a3d47ea9a571dd8e6fc47c50e4eff769
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-03-02 10:07:58 +0100

    Rozwiązania

--- 4IB Grupa 2/2023.03.02 Funkcje.py
+++ 4IB Grupa 2/2023.03.02 Funkcje.py
@@ -41,7 +41,46 @@ print(SumaEl([2,3,4,65,4,3,4,5,6]))
 
 
 #1. Zdefinuj funkcję, która liczy sumę dwóch liczb
+def Sum(a,b):
+    return float(a) + float(b)
+
 #2. Zdefinuj funkcję, która sprawdza znak liczby i wypisuje odpowiednią informację na ekran
+def Sign(a):
+    if a>0: print("Dodatnia")
+    elif a<0: print("Ujemna")
+    else: print("Zero")
+
 #3. Zdefinuj funkcję, która sprawdza czy z podanych długości odcinków można utworzyć trójkąt (False/True)
+def Triangle(a,b,c):
+    if a>b+c and b>a+c and c>a+b: return True
+    else: return False
+
 #4. Zdefinuj funkcję, która sprawdza czy podana liczba jest pierwsza
+def Prime(a):
+    if a<2: return False
+    for i in range(2, int((a**.5)+1)):
+        if a%i == 0: return False
+    return True
+
+for i in range(100):
+    print(i,Prime(i))
+
 #5. Zdefinuj funkcję, która zwraca NWD dwóch liczb
+def NWD(a,b):
+    while(a!=b):
+        if a>b: a-=b
+        else: b-=a
+    return a
+
+print(NWD(5,5))
+
+def NWD2(a,b):
+    while True:
+        r = a%b
+        if r == 0: return b
+        else: 
+            a=b
+            b=r
+    return b
+
+print(NWD2(51,15))
