0b1ff2add501b547421b3e685136635982b961b5
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-03-10 12:39:38 +0100

    Zadania

---
+++ 4IA Grupa 1/2023.03.10 zadania.py
@@ -0,0 +1,10 @@
+
+
+
+#1. Napisz funkcję, która zwraca sumę elementów listy (krotki)
+#2. Napisz funkcję, która zwraca średnią elementów listy (krotki)
+#3. Napisz funkcję, która sprawdza (zwraca true / false) czy z podanych długości odcinków można zbudować trójkąt
+#4. Napisz funkcję, która sprawdza (zwraca true / false) czy liczba jest pierwsza
+#5a. Napisz funkcję, która oblicza NWD dwóch liczb
+#5b. Napisz funkcję, która oblicza NWW dwóch liczb
+#6. Napisz funkcję, która zwraca listę dzielników liczby (faktoryzacja).
