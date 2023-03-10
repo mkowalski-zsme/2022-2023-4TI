6fd58b69ecc207b1544f968cd7dc0b5611987c62
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-03-10 13:16:39 +0100

    Update

--- 4IA Grupa 1/2023.03.10 zadania.py
+++ 4IA Grupa 1/2023.03.10 zadania.py
@@ -1,10 +1,34 @@
 
+#Rozwiązanie zadania z kartkówki:
+'''
+a = int(input())
+b = int(input())
 
+if a % 2 == 0 or b % 2 == 0:
+    print(a+b)
+'''
 
 #1. Napisz funkcję, która zwraca sumę elementów listy (krotki)
+
+def suma(arg):
+    pass
+
 #2. Napisz funkcję, która zwraca średnią elementów listy (krotki)
 #3. Napisz funkcję, która sprawdza (zwraca true / false) czy z podanych długości odcinków można zbudować trójkąt
 #4. Napisz funkcję, która sprawdza (zwraca true / false) czy liczba jest pierwsza
 #5a. Napisz funkcję, która oblicza NWD dwóch liczb
 #5b. Napisz funkcję, która oblicza NWW dwóch liczb
 #6. Napisz funkcję, która zwraca listę dzielników liczby (faktoryzacja).
+
+def faktoryzacja(liczba):
+    r = []
+
+    return r
+
+"""
+faktoryzacja(5) -> [5]
+faktoryzacja(15) -> [3,5]
+faktoryzacja(20) -> [2,2,5]
+faktoryzacja(25) -> [5,5]
+
+"""
