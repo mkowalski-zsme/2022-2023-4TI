361e4435c45815b44421d2320962dfdeb5db742e
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-04-05 11:34:58 +0200

    Update

---
+++ 4IB Grupa 1/2023.04.05 app.py
@@ -0,0 +1,31 @@
+def main():
+    # import random
+
+    # print(random.random())
+
+    # print(random.randint(0,100))
+
+    # import random as rnd
+
+    # print(rnd.random())
+
+    # from random import *
+
+    # print(random())
+    # print(randint(1,100))
+
+    # from random import random
+
+    # print(random())
+    # print(randint(1,100)) #error
+
+    import funkcje
+    funkcje.Hello("Jan")
+
+    # from funkcje import *
+    # Hello("Marcin") 
+
+
+
+if __name__ == "__main__":
+    main()
