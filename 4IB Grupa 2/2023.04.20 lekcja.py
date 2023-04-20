6321bf217926199fa6341d0bf1e0d95595372a39
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-04-20 10:08:53 +0200

    Update

---
+++ 4IB Grupa 2/2023.04.20 lekcja.py
@@ -0,0 +1,42 @@
+import math
+import numpy
+import pandas as pd
+# print(math.ceil(3.15))
+# print(math.floor(3.15))
+
+from math import ceil, floor, exp
+# print(ceil(3.14))
+
+from math import *
+# print(sqrt(16))
+# import biblioteka as lib
+from biblioteka import Iloraz
+
+
+def moja_funkcja():
+    pass
+
+def main():
+    print(Iloraz(3,4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ 4IB Grupa 2/biblioteka.py
@@ -0,0 +1,10 @@
+def Suma(a,b):
+    return a+b
+
+def Iloraz(a,b):
+    if b == 0: return None
+    return a/b
+
+if __name__ == "__main__":
+    print(Iloraz(3,0))
+    print(Iloraz(3,2))
