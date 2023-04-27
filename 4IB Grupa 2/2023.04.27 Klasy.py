39df6f44fa7a98bcc8d5cc733837f7882cfb14d7
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-04-27 10:20:22 +0200

    Update

--- 4IB Grupa 2/2023.04.27 Klasy.py
+++ 4IB Grupa 2/2023.04.27 Klasy.py
@@ -5,7 +5,8 @@ class Osoba:
         self.wiek = wiek
     
     def __str__(self):
-        return self.imie+" "+self.nazwisko+" "+str(self.wiek)
+        # return self.imie+" "+self.nazwisko+" "+str(self.wiek)
+        return f"{self.imie} {self.nazwisko} {self.wiek}"
     
     def Wypisz(self):
         print(self.imie,self.nazwisko,self.wiek)
@@ -20,7 +21,12 @@ print(p1,p2,sep="\n")
 p1.Wypisz()
 p2.Wypisz()
 
+zm1 = "tekst"
+zm2 = 123
+zm3 = True
 
+print(zm1+" "+str(zm2)+" dsdsads "+ str(zm3))
+print(f"{zm1} {zm2} dsdsads {zm3}")
 
 # print(p1.imie,p1.nazwisko,p1.wiek)
 # p1.wiek = 33
