a46d6ced8d3eb5b074b9bf09a5626e6086083230
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-04-27 09:19:30 +0200

    Update

---
+++ 4IA Grupa 2/2023.04.20 lekcja.py
@@ -0,0 +1,29 @@
+# import math
+# print(math.ceil(2.13))
+# import math as mt
+# print(mt.ceil(2.13))
+
+
+# from random import randint, random
+# print(randint(3, 16))
+# print(random())
+
+
+# from random import *
+# print(random())
+# print(randint(3,123))
+
+
+# from math import *
+# from numpy import *
+
+
+import biblioteka as lib
+# from biblioteka import Suma
+
+if __name__ == "__main__":
+    print(lib.Suma(3,123))
+
+
+
+
